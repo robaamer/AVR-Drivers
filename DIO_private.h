0a3d396bb99c4001a02b21ba9d86966441283bdc
robaamer <152691986+robaamer@users.noreply.github.com>
2024-06-24 19:13:58 +0300

    DIO_private.h

---
+++ DIO_private.h
@@ -0,0 +1,35 @@
+/***************************  Layer: MCAL****************************/
+/****************************Author: Roba****************/
+/****************************Version:01*********/
+/****************************Date: 16/8/2023 ******************/
+/****************************Component:DIO*************/
+/*******************************************************/
+
+#ifndef DIO_PRIVATE_H_
+#define DIO_PRIVATE_H_
+
+#define PORTA                   *((volatile u8*)0X3B)
+#define DDRA                    *((volatile u8*)0X3A)
+#define PINA                     *((volatile u8*)0X39)
+
+
+#define PORTB                   *((volatile u8*)0X38)
+#define DDRB                    *((volatile u8*)0X37)
+#define PINB                     *((volatile u8*)0X36)
+
+
+#define PORTC                   *((volatile u8*)0X35)
+#define DDRC                    *((volatile u8*)0X34)
+#define PINC                    *((volatile u8*)0X33)
+
+#define PORTD                   *((volatile u8*)0X32)
+#define DDRD                    *((volatile u8*)0X31)
+#define PIND                    *((volatile u8*)0X30)
+
+
+
+
+
+
+
+#endif
